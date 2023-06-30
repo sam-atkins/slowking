9a5df4ead9f629f950d657f071d5c11d95a88627
Sam Atkins <butlers.tunic.04@icloud.com>
2023-06-30 08:23:42 +0100

    http client prep

--- src/adapters/http_client.py
+++ src/adapters/http_client.py
@@ -1,3 +1,12 @@
 """
-Placeholder an http client adapter to communicate with the benchmark target instance.
+HTTP client adapter to communicate with the benchmark target instance.
+
+For retries: https://tenacity.readthedocs.io/en/latest/
+@retry(wait=wait_fixed(3) + wait_random(0, 2))
+@retry(stop=(stop_after_delay(10) | stop_after_attempt(5)))
+combine some variation of the 2 above
 """
+
+
+class HttpClient:
+    pass
