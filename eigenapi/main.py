324fc6f8ae33c25e680e1dbb50e0f0df38a96b7c
Sam Atkins <butlers.tunic.04@icloud.com>
2023-07-01 08:02:38 +0100

    add mock endpoints

--- eigenapi/main.py
+++ eigenapi/main.py
@@ -1,6 +1,8 @@
 """
 Mock Eigen API
 """
+import random
+
 from fastapi import FastAPI, Response
 
 app = FastAPI()
@@ -11,13 +13,20 @@ def health():
     return {"status": "ok"}
 
 
-# endpoints:
-# auth token: auth/v1/token/
-# create project
-# url = urljoin({base_url}api/project_management/v2/, "projects")
-# doc uploader: url = f"{self.base_url_v1}document_uploader/"
-
-
 @app.get("/auth/v1/token/")
 def auth_token():
     return Response(content="token")
+
+
+@app.post("/api/project_management/v2/projects")
+def create_project():
+    project_id = random.randint(1, 100)
+    # TODO data shape of the response?
+    data = {"project_id": project_id}
+    return Response(content=data)
+
+
+@app.post("api/v1/document_uploader/")
+def document_uploader():
+    # TODO add background task to "process" documents
+    return Response(content="document uploaded")
