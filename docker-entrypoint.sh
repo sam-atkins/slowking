2f92782feb9e31fa1ee75bb4e3e389e5fee63f32
Sam Atkins <butlers.tunic.04@icloud.com>
2023-07-07 21:38:33 +0100

    remove waiting for db for API

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,10 +14,6 @@ if [[ "$1" == "event-consumer" ]]; then
     alembic upgrade head
     watchmedo auto-restart --directory=/home/app/slowking --pattern="*.py" -- python -m slowking.entrypoints.event_consumer
 elif [[ "$1" == "api" ]]; then
-    python db_ready.py
-    # NOTE: we need to wait for the event consumer to run migrations before starting the api
-    # this is a hack which is okish for dev, but for prod we would need a more robust solution
-    sleep 5
     uvicorn slowking.router:app --host 0.0.0.0 --reload --port 8091
 else
     echo "Unknown or missing sub-command: '$1'"
