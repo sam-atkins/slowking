1f3a6107c699a6120ab6a34014c643bf2e45877b
Sam Atkins <butlers.tunic.04@icloud.com>
2023-07-12 09:34:15 +0100

    http endpoint unhappy path tests

--- tests/integration/slowking/entrypoints/test_http.py
+++ tests/integration/slowking/entrypoints/test_http.py
@@ -1,6 +1,8 @@
 from http import HTTPStatus
 from unittest.mock import patch
 
+import pytest
+from fastapi.exceptions import RequestValidationError
 from fastapi.testclient import TestClient
 
 from slowking.entrypoints.http import router
@@ -38,6 +40,19 @@ def test_start_benchmark_endpoint_accepted(mock_add_task):
     assert response.status_code == HTTPStatus.ACCEPTED
 
 
+def test_start_benchmark_endpoint_unprocessable_entity():
+    payload = {
+        "name": "test-benchmark",
+        "benchmark_type": "latency",
+        "target_infra": "k8s",
+        "target_url": "localhost",
+        "target_eigen_platform_version": "5.11.0-rc.1",
+    }
+    with pytest.raises(RequestValidationError):
+        response = client.post("/api/v1/benchmarks/start", json=payload)
+        assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
+
+
 @patch("fastapi.BackgroundTasks.add_task")
 def test_update_document_endpoint_accepted_with_start_time(mock_add_task):
     payload = {
@@ -62,3 +77,14 @@ def test_update_document_endpoint_accepted_with_end_time(mock_add_task):
     }
     response = client.post("/api/v1/benchmarks/documents", json=payload)
     assert response.status_code == HTTPStatus.ACCEPTED
+
+
+def test_update_document_endpoint_unprocessable_entity():
+    payload = {
+        "document_name": "test doc",
+        "eigen_project_id": "22",
+        "benchmark_host_name": "test host",
+    }
+    with pytest.raises(RequestValidationError):
+        response = client.post("/api/v1/benchmarks/documents", json=payload)
+        assert response.status_code == HTTPStatus.UNPROCESSABLE_ENTITY
